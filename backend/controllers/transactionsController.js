01479cd412bb4470886b4cf62e3cfeeb5e52d7ed
Luiz Gomes <luizgomes@tangerino.com.br>
2020-07-14 20:32:40 -0300

    Criando rota para criar novas entradas de lançamentos

--- backend/controllers/transactionsController.js
+++ backend/controllers/transactionsController.js
@@ -17,12 +17,12 @@ export const everyMonthRegistred = async (req, res) => {
   }
 };
 
-export const monthlyFilter = async (req, res) => {
+export const dataMonthlyFilter = async (req, res) => {
   try {
     const yearMonth = req.params.yearMonth;
     const response = await transactionModel.find({ yearMonth: yearMonth });
 
-    const totalReleases = response.length;
+    const totalEntries = response.length;
 
     const totalRecipes = response
       .filter((entry) => {
@@ -43,14 +43,43 @@ export const monthlyFilter = async (req, res) => {
     const finalBalance = totalRecipes - totalExpenses;
 
     const object = {
-      totalReleases: formatCurrency(totalReleases),
+      totalEntries: formatCurrency(totalEntries),
       totalRecipes: formatCurrency(totalRecipes),
       totalExpenses: formatCurrency(totalExpenses),
       finalBalance: formatCurrency(finalBalance),
-      releases: response,
+      Entries: response,
     };
 
     res.status(200).send(object);
+  } catch (error) {
+    logger.info(error);
+    res.status(500).send({
+      message: error,
+    });
+  }
+};
+
+export const createEntry = async (req, res) => {
+  try {
+    const { description, value, category, type, date } = req.body;
+
+    date = date.replace("/", "-");
+
+    // const entry = {
+    //   description: description,
+    //   value: parseInt(value),
+    //   category: category,
+    //   year: Number,
+    //   month: Number,
+    //   day: Number,
+    //   yearMonth: String,
+    //   yearMonthDay: String,
+    //   type: type,
+    // };
+
+    // const response = await transactionModel.create(entry);
+
+    res.status(200).send(date);
   } catch (error) {
     logger.info(error);
     res.status(500).send({
--- backend/routes/transactionsRouter.js
+++ backend/routes/transactionsRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
   everyMonthRegistred,
-  monthlyFilter,
+  dataMonthlyFilter,
+  createEntry,
 } from "../controllers/transactionsController.js";
 
 const transactionsRouter = express.Router();
 
 transactionsRouter.get("/everyMonthRegistred", everyMonthRegistred);
-transactionsRouter.get("/monthlyFilter/:yearMonth", monthlyFilter);
+transactionsRouter.get("/monthlyFilter/:yearMonth", dataMonthlyFilter);
+transactionsRouter.post("/createEntry", createEntry);
 
 export default transactionsRouter;
